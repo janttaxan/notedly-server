666d0de070e28c1e173c2ba58ee6d4623d07b699
Maxim Frolov <buterbrodhs@gmail.com>
2021-09-21 00:49:19 +0300

    base settings for apollo-server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,37 @@
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('hello express + TypeScript');
-});
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const resolvers = {
+  Query: {
+    hello: () => 'Hello apollo server',
+  },
+};
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+async function start() {
+  try {
+    await server.start();
+    server.applyMiddleware({ app, path: '/api' });
+    app.get('/', (req, res) => {
+      res.send('hello express + TypeScript');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`GraphQL Server running at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  } catch (err) {
+    console.log('Server error:', String(err));
+  }
+}
+
+start();
