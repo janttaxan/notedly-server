4474bf29cf1fcdcfbf361416c0004205be18fadd
Maxim Frolov <buterbrodhs@gmail.com>
2021-09-21 00:09:38 +0300

    add typescript

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('hello express + TypeScript');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
